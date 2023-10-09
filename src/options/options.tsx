0a227d6427005d952a9d83842ee5f26be300e5ec
Vivek Vaibhav <vivek@oriserve.com>
2023-10-09 18:36:05 +0530

    options page base design updated

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -6,8 +6,12 @@ import {
   responsiveFontSizes,
 } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
-import Paper from "@mui/material/Paper"
 import Container from "@mui/material/Container"
+import Paper from "@mui/material/Paper"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
 
 import "../config/common"
 import "./options.css"
@@ -23,7 +27,36 @@ const Options: React.FC<{}> = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="sm">
-        <Paper sx={{ p: 2, my: 4 }}>hello options</Paper>
+        <Paper sx={{ p: 2, my: 4 }}>
+          <Box
+            component="img"
+            src="./images/icon-128.png"
+            alt="weather"
+            height={100}
+            display="block"
+            mx="auto"
+            my={2}
+          />
+          <Typography variant="h4" align="center" fontWeight="fontWeightMedium">
+            Weather
+          </Typography>
+          <Typography
+            component="p"
+            align="center"
+            color="text.secondary"
+            mb={4}
+          >
+            Access Weather Updates for Any Location Simply by Name
+          </Typography>
+          <TextField
+            size="small"
+            placeholder="Enter your home city name"
+            fullWidth
+          />
+          <Box pt={4} display="flex" justifyContent="center">
+            <Button variant="contained">Save</Button>
+          </Box>
+        </Paper>
       </Container>
     </ThemeProvider>
   )
